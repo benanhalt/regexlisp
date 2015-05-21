dd424a0bca1008ce9014e18fdef7f818c77ddb0d
Ben Anhalt <anhalt@ku.edu>
2015-05-21 15:36:22 -0500

    works more or less.

--- regex.lisp
+++ regex.lisp
@@ -9,7 +9,8 @@
   (make-instance 'nfa :start-state start :accepting-state accept))
 
 (defclass nfa-state ()
-  ((transitions :initarg :transitions :accessor transitions)))
+  ((transitions :initarg :transitions :accessor transitions)
+   (entering :initform (lambda (pos) pos) :accessor entering)))
 
 (defun make-state (&rest transitions)
   (make-instance 'nfa-state :transitions transitions))
@@ -68,7 +69,7 @@
 (defun epsilon-closure (states)
   (loop
      with new-states = states
-     for current-states = states then (append current-states new-states)
+     for current-states = states then (remove-duplicates (append current-states new-states))
      do (setf new-states (remove-if (lambda (s) (member s current-states))
                                     (mapcan #'epsilon-states new-states)))
      until (null new-states)
@@ -86,12 +87,65 @@
    (mapcan (lambda (state) (follow-transitions state input))
            states)))
 
+(defun enter-states (states pos)
+  (mapc (lambda (s) (funcall (entering s) pos)) states)
+  states)
+
 (defun run-nfa (nfa input-string)
   (loop
-     with states = (epsilon-closure (list (start-state nfa)))
-     for input across input-string do
-       (setf states (reachable-states states input))
+     with states = (enter-states (epsilon-closure (list (start-state nfa))) 0)
+     for input across input-string
+     for i upfrom 0
+     do
+       (setf states (enter-states (reachable-states states input) i))
        (if (null states) (return :reject))
      finally (return (if (member (accepting-state nfa) states)
                          :accept
                          :reject))))
+
+
+
+(defvar *pos*)
+
+(defun nfa-from-pattern (pattern)
+  (let* ((*pos* 0)
+         (nfa (parse-pattern pattern)))
+    (unless (eql *pos* (length pattern))
+      (error "unexpected end of pattern"))
+    nfa))
+
+(defun parse-pattern (pattern)
+  (loop
+     with nfas = '()
+     do (case (elt pattern *pos*)
+          (#\* (incf *pos*)
+               (push (nfa-star (pop nfas)) nfas))
+
+          (#\| (incf *pos*)
+               (setf nfas (list
+                           (nfa-alternative (reduce #'nfa-concat (reverse nfas)) (parse-pattern pattern)))))
+
+          (#\( (incf *pos*)
+               (let ((start-pos *pos*))
+                 (push (make-capture (parse-pattern pattern) (subseq pattern start-pos *pos*)) nfas))
+               (unless (and (< *pos* (length pattern)) (eql (elt pattern *pos*) #\)))
+                 (error (format nil "expected ) at ~d" *pos*)))
+               (incf *pos*))
+
+          (#\) (loop-finish))
+
+          (t (push (nfa-atom (elt pattern *pos*)) nfas)
+             (incf *pos*)))
+
+       until (>= *pos* (length pattern))
+       finally (return (reduce #'nfa-concat (reverse nfas)))))
+
+
+
+(defun make-capture (nfa name)
+  (let (start)
+    (setf (entering (start-state nfa))
+          (lambda (pos) (setf start pos)))
+    (setf (entering (accepting-state nfa))
+          (lambda (pos) (print (list name start pos)))))
+  nfa)
